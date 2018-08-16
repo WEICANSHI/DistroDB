0abe4032df4b496d64fd72a5aa9ec738a372e6de
WEICANSHI <weic2@rpi.edu>
2018-08-15 23:46:52 -0400

    Update TestN3.java

--- Source/Test/TestN3.java
+++ Source/Test/TestN3.java
@@ -6,7 +6,7 @@ public class TestN3 {
 	public static void main(String args[]) {
 		Node node3 = new Node(3333);
 		node3.start();
-		node3.ConnectDB("root", "Yy994948350", "Market_db03", "Market_list", "jdbc:mysql://localhost:3306/");
+		node3.ConnectDB("username", "password", "database name", "table name", "url to database");
 	}
 }
 
