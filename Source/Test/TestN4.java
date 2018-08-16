1eeae93da05adb05b7004a4391bd2503a613fb76
WEICANSHI <weic2@rpi.edu>
2018-08-15 23:47:15 -0400

    Update TestN4.java

--- Source/Test/TestN4.java
+++ Source/Test/TestN4.java
@@ -4,8 +4,8 @@ import distroDB.Node;
 
 public class TestN4 {
 	public static void main(String args[]) {
-		Node node3 = new Node(4444);
-		node3.start();
-		node3.ConnectDB("root", "Yy994948350", "Market_db04", "Market_list", "jdbc:mysql://localhost:3306/");
+		Node node4 = new Node(4444);
+		node4.start();
+		node4.ConnectDB("username", "password", "database name", "table name", "url to database");
 	}
 }
