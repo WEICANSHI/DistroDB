37f5fbe1169ee989fd0c4109aebc6530470e83bd
WEICANSHI <weic2@rpi.edu>
2018-08-15 23:46:13 -0400

    Update TestN2.java

--- Source/Test/TestN2.java
+++ Source/Test/TestN2.java
@@ -6,6 +6,6 @@ public class TestN2 {
 	public static void main(String args[]) {
 		Node node2 = new Node(2222);
 		node2.start();
-		node2.ConnectDB("root", "Yy994948350", "Market_db02", "Market_list", "jdbc:mysql://localhost:3306/");
+		node2.ConnectDB("username", "password", "database name", "table name", "url to database");
 	}
 }
